d12cec1e3f4eec5d31a2786f5287673381b1cb1a
Pablo García <development@musicmonsta.com>
2019-07-20 02:32:11 +0200

    Custom AuthController for Uloyola authentication

--- src/AuthController.php
+++ src/AuthController.php
@@ -4,6 +4,8 @@ namespace Metrogistics\AzureSocialite;
 
 use Illuminate\Routing\Controller;
 use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
@@ -12,9 +14,22 @@ class AuthController extends Controller
         return Socialite::driver('azure-oauth')->redirect();
     }
 
-    public function handleOauthResponse()
+    public function handleOauthResponse(Request $request)
     {
+    	Log::debug('>>handleOauthResponse>> input:\n' . print_r($request->all(), true));
         $user = Socialite::driver('azure-oauth')->user();
+        
+        Log::debug('Tenemos respuesta de Azure');
+
+        /*
+        foreach($user as $var => $value){
+        	Log::debug("AzureUser->$var: $value");
+        }
+        */
+        
+        //TO-DO Weinx: aquí no debería hacerse la autenticación. Habría que hacer lo que ahora hace la API a la hora de obtener un token y devolverlo a la App.
+        Log::debug(print_r($user, true));
+        
 
         $authUser = $this->findOrCreateUser($user);
 
@@ -24,9 +39,11 @@ class AuthController extends Controller
         //     'azure_user' => $user
         // ]);
 
-        return redirect(
+        echo "Autenticado como ". $authUser->email;
+        
+        /* return redirect(
             config('azure-oath.redirect_on_login')
-        );
+        ); */
     }
 
     protected function findOrCreateUser($user)
